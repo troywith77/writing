aee699a16b389c68dc43c6df448f26c35a73f1e2
troywith77 <ruitang307@gmail.com>
2019-06-24 14:13:46 +0800

    read file from plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,13 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     'gatsby-plugin-emotion'
   ]
 }
